fbfed934284b6d41be1ba376d4eb012ba6dfa882
Brian Regalado <brianregalado2564@gmail.com>
2024-09-08 21:49:03 -0500

    corregir errores para el despliegue en vercel

--- src/components/planes/Plans.tsx
+++ src/components/planes/Plans.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react'
 import PricesPlans from './PricesPlans/PricesPlans'
-import { PricesPlansProps } from './PricesPlans/PricesPlans.types'
 
 function Plans() {
     const [isMonthly, setIsMonthly] = useState(true)
